3e3574a65a9487df32462c22c029da34cd23f8ec
venkattj <venkattejavasa08@gmail.com>
2019-07-13 17:43:36 +0530

    courses added

---
+++ app/src/main/java/com/example/team6/Courses.java
@@ -0,0 +1,60 @@
+package com.example.team6;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+public class Courses extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    ImageButton sk,tsk ,others;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+        mAuth = FirebaseAuth.getInstance();
+        sk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Courses.this,Softskills.class);
+                startActivity(intent);
+            }
+        });
+        tsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Courses.this,Techskills.class);
+                startActivity(intent);
+            }
+
+        });
+        others.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Courses.this,Otherskills.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+
+
+}
