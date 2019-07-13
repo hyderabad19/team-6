b4185b417276c5efacccabd56c9915c728535fca
saisahiti <saisahiti242@gmail.com>
2019-07-13 23:38:02 +0530

    Integrate the activities of all courses

---
+++ app/src/main/java/com/example/team6/bigdata.java
@@ -0,0 +1,13 @@
+package com.example.team6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bigdata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bigdata);
+    }
+}
