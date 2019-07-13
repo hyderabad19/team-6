9b253550a305349ba23aaad611479fc18b408ab3
saisahiti <saisahiti242@gmail.com>
2019-07-14 03:32:06 +0530

    Added quiz files

--- app/src/main/java/com/example/team6/mlquiz.java
+++ app/src/main/java/com/example/team6/mlquiz.java
@@ -3,11 +3,11 @@ package com.example.team6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class gdquiz extends AppCompatActivity {
+public class mlquiz extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gdquiz);
+        setContentView(R.layout.activity_mlquiz);
     }
 }
