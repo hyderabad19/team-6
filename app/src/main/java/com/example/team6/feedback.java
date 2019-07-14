ecc820496cd7cdca395b47c225f9d67ebb19c2f3
saisahiti <saisahiti242@gmail.com>
2019-07-14 09:52:35 +0530

    Feedback files

--- app/src/main/java/com/example/team6/feedback.java
+++ app/src/main/java/com/example/team6/feedback.java
@@ -3,11 +3,11 @@ package com.example.team6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class registertrainee extends AppCompatActivity {
+public class feedback extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registertrainee);
+        setContentView(R.layout.activity_feedback);
     }
 }
